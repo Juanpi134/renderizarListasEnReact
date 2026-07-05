e6904ad724e2c709d2038104ff098fa5dfb70692
Juanpi134 <juanpitocino_15@hotmail.com>
2026-07-04 23:40:13 -0300

    se añade un comentario como debería quedar el array de strings en li

--- src/componentes/List.jsx
+++ src/componentes/List.jsx
@@ -2,6 +2,16 @@ function List(){
     // Creacion de array de frutas
     const frutas = ["manzana","naranja","banana","piña"];
 
+    // Necesitamos convertir el array de string en un array de li items
+    /*  
+    
+    <li>manzana</li>
+    <li>naranja</li>
+    <li>piña</li>
+    <li>banaana</li>
+    */
+
+
     return (frutas);
 }
 
