b04272e2e749390f465823e021390b27ca10543e
Juanpi134 <juanpitocino_15@hotmail.com>
2026-07-04 23:46:22 -0300

    se crea comentarios y tambien se añade como quedaría el array de strings en una lista desordenada

--- src/componentes/List.jsx
+++ src/componentes/List.jsx
@@ -11,8 +11,28 @@ function List(){
     <li>banaana</li>
     */
 
+    //recorro cada elemento y creo un elemento li
+    const elementosDeLista = frutas.map((fruta) => {
+         return <li>{fruta}</li>
+    })
+   
 
-    return (frutas);
+   
+
+   /*
+   console.log(elementosDeLista);
+
+   Array(4):
+   0:manzana,
+   1:naranja,
+   2:banana,
+   3:piña
+     */
+
+   //retornamos una lista desordenada con los elementos creados en mi array de frutas que recorrí con map
+
+   return (<ul>{elementosDeLista}</ul>);
+
 }
 
 export default List
